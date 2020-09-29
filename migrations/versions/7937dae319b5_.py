a808dae348b528281b4d6c3cd99d387209facac3
ramyaragupathy <r.ragupathy85@gmail.com>
2020-09-29 15:07:49 +0530

    Handle islands and special chars

--- migrations/versions/7937dae319b5_.py
+++ migrations/versions/7937dae319b5_.py
@@ -14,41 +14,51 @@ revision = "7937dae319b5"
 down_revision = "ba778ef9c615"
 branch_labels = None
 depends_on = None
+nominatim_base_url = "https://nominatim.openstreetmap.org"
+nominatim_data_format = "&format=jsonv2&accept-language=en"
 
 
 def upgrade():
     conn = op.get_bind()
-    fetch_country_combinations = (
+    # fetch existing country names
+    fetch_countries = (
         "select distinct(unnest(country)) from projects where country is not null;"
     )
-    country_combinations = conn.execute(fetch_country_combinations)
-    for country in country_combinations:
+    countries = conn.execute(fetch_countries)
+    for country in countries:
         country = country[0]
-        print(country)
-        url = (
-            "https://nominatim.openstreetmap.org/search?country="
-            + country
-            + "&format=jsonv2&accept-language=en"
-        )
-        print(url)
+        # search by name
+        url = nominatim_base_url + "/search?country=" + country + nominatim_data_format
         country_search = requests.get(url).json()
-        print(country_search)
-        print(country_search[0].get("display_name"))
+        if country_search == []:
+            url = nominatim_base_url + "/search?q=" + country + nominatim_data_format
+            country_search = requests.get(url).json()
         lat = country_search[0].get("lat")
         lng = country_search[0].get("lon")
-        print(lat, lng)
-        print("search done")
-        url = "{0}/reverse?format=jsonv2&lat={1}&lon={2}&accept-language=en".format(
-            "https://nominatim.openstreetmap.org", lat, lng
+        # reverse geocode to fetch English names
+        url = "{0}/reverse?lat={1}&lon={2}{3}".format(
+            nominatim_base_url, lat, lng, nominatim_data_format
         )
-        print(url)
         country_info = requests.get(url).json()
-        if country_info is not None:
-            print(country_info)
-        # update_project = "update projects set country ="+ country_name + "where country=" + country + ";"
-        # conn.execute(update_project)
-        # print(country_search)
-        # if country_search:
+        if country_info != []:
+            updated_country_name = country_info.get("address").get("country")
+            if country != updated_country_name:
+                special_char = updated_country_name.find("'")
+                if special_char >= 0:
+                    updated_country_name = (
+                        updated_country_name[:special_char]
+                        + "'"
+                        + updated_country_name[special_char:]
+                    )
+                update_project = (
+                    "update projects set country = '{\""
+                    + updated_country_name
+                    + "\"}' where country @> ARRAY['"
+                    + country
+                    + "']::varchar[]"
+                    + ";"
+                )
+                conn.execute(update_project)
 
 
 def downgrade():
